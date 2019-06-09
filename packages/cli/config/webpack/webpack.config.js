f2c959b0be26394320761cb1864f82b0e3252f67
munaibh <munaibh@gmail.com>
2019-06-09 15:05:00 +0100

    Add webpack and babel config.

---
+++ packages/cli/config/webpack/webpack.config.js
@@ -0,0 +1,73 @@
+/**
+ * Import Dependencies
+ */
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const babelConfig = require('./babelConfig')
+
+/**
+ * Configure CSS Extraction
+ * Extract style.css for both editor and frontend styles.
+ */
+const extractFrontendStyles = new ExtractTextPlugin({ filename: 'style.build.css' })
+const extractEditorStyles = new ExtractTextPlugin({ filename: 'editor.build.css' })
+const extractConfig = {
+	use: [
+    { loader: 'css-loader' },
+		{
+			loader: 'sass-loader',
+			options: {
+				data: '@import "./client/common/common.scss";\n',
+				outputStyle: 'nested',
+			}
+		}
+	]
+}
+
+/**
+ * Export Configuration
+ * Export the configuration object to be consumed by webpack.
+ */
+module.exports = {
+  mode: "development",
+  context: __dirname,
+	entry: [
+    'webpack/hot/dev-server',
+    process.cwd() + '/client/blocks.js'
+  ],
+	output: {
+		chunkFilename: 'block.[chunkhash].js',
+    filename: 'block.build.js',
+    path: process.cwd() + '/build',
+    publicPath: "http://localhost:9000/"
+  },
+  watch: false,
+  stats: "errors-only",
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx|mjs)$/,
+				exclude: /(node_modules)/,
+				use: {
+          loader: 'babel-loader',
+          options: babelConfig,
+				},
+			},
+			{
+				test: /style\.(css|sass|scss)$/,
+				exclude: /(node_modules)/,
+				use: extractFrontendStyles.extract(extractConfig),
+			},
+			{
+				test: /editor\.(css|sass|scss)$/,
+				exclude: /(node_modules)/,
+				use: extractEditorStyles.extract(extractConfig),
+			}
+		]
+	},
+	plugins: [
+    extractFrontendStyles, 
+    extractEditorStyles,
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+}
