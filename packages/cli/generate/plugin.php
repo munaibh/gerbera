ec0b414922872c5ec58c2912a1e86f29f7abfb1b
munaibh <munaibh@gmail.com>
2019-06-09 17:45:04 +0100

    Update "plugin.php" to have wordpress comment.

---
+++ packages/cli/generate/plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: <%= name %>
+ * Plugin URI: https://github.com/munaibh/gerbera
+ * Description: An opinionated CLI to scaffold Gutenberg blocks that are easy to customize.
+ * Author: Munaib Hussain
+ * Author URI: https://munaibh.com
+ * Version: 0.0.2
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package Gerbera
+ */
+
+require_once plugin_dir_path( __FILE__ ) . 'server/blocks.php';
