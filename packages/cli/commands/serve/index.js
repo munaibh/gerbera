7dbf44e5060d1eae5bce5c90888d989edf12c002
munaibh <munaibh@gmail.com>
2019-06-09 18:26:43 +0100

    Add `webpack-dev-server` and configure hot reloading.

--- packages/cli/commands/serve/index.js
+++ packages/cli/commands/serve/index.js
@@ -1,4 +1,8 @@
 const path = require('path')
+const WebpackDevServer = require('webpack-dev-server')
+const webpack = require('webpack')
+const WebpackConfig = require('../../config/webpack/webpack.config')
+const webpackMessager = require('../utils/webpackMessager.js')
 const { spawn } = require('child_process')
 
 const startDocker = () => {  
@@ -8,7 +12,28 @@ const startDocker = () => {
   docker.stderr.on('data', (data) => console.log(`stderr: ${data}`))
 }
 
+const startDevServer = () => {
+  const compiler = webpack(WebpackConfig)
+  const messager = webpackMessager(compiler)
+
+  const server = new WebpackDevServer(compiler, {
+    hot: true,
+    inline: true,
+    host: '0.0.0.0',
+    port: 9000,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    disableHostCheck: true,
+    quiet: true,
+    contentBase: process.cwd() + '/build'
+  })
+
+  server.listen(9000)
+}
+
 module.exports = (args, rawArgs) => {
   const useDocker = args.includes('--docker')
   useDocker && startDocker()
+  startDevServer()
 }
