165a914daaa5e62138f3a1f7be941377eafa643e
munaibh <munaibh@gmail.com>
2019-06-09 16:03:34 +0100

    And thus, we were blessed with Docker.

--- packages/cli/commands/serve/index.js
+++ packages/cli/commands/serve/index.js
@@ -1,3 +1,13 @@
+const path = require('path')
+const { spawn } = require('child_process')
+
+const startDocker = () => {  
+  var dockerConfig = path.join(__dirname,'..', '..', 'config', 'docker', 'docker-compose.yml')
+  var docker = spawn(`docker-compose`, ['-f', `${dockerConfig}`, '--project-directory', process.cwd(), 'up']);
+  docker.stdout.on('data', (data) => console.log(`stdout: ${data}`))
+  docker.stderr.on('data', (data) => console.log(`stderr: ${data}`))
+}
+
 module.exports = (args, rawArgs) => {
-  console.log('Create!', args, rawArgs)
+  startDocker()
 }
