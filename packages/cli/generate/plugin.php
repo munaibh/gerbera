9520758aacc8488041617be685ee5e9b24a61ea3
munaibh <munaibh@gmail.com>
2019-06-10 09:04:14 +0100

    Bump to 0.0.4

--- packages/cli/generate/plugin.php
+++ packages/cli/generate/plugin.php
@@ -5,7 +5,7 @@
  * Description: An opinionated CLI to scaffold Gutenberg blocks that are easy to customize.
  * Author: Munaib Hussain
  * Author URI: https://munaibh.com
- * Version: 0.0.3
+ * Version: 0.0.4
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
