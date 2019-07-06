41bd34938c716e48724a69289da08d0e7a6b6e39
munaibh <munaibh@gmail.com>
2019-07-06 14:23:53 +0100

    Lower case `webpackConfig` and set mode to production in build command.

--- packages/cli/commands/build/index.js
+++ packages/cli/commands/build/index.js
@@ -1,9 +1,10 @@
 const webpack = require('webpack')
-const WebpackConfig = require('../../config/webpack/webpack.config')
+const webpackConfig = require('../../config/webpack/webpack.config')
 const webpackMessager = require('../utils/webpackMessager.js')
 
 module.exports = () => {
-  const compiler = webpack(WebpackConfig)
+  webpackConfig.mode = 'production'
+  const compiler = webpack(webpackConfig)
   const messager = webpackMessager(compiler)
   compiler.run()
 }
